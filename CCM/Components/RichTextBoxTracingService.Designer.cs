d2b3325716013807b8a1bb7429408ab06c992fe3
JordanBoltonMN <jordanboltonmn@gmail.com>
2025-02-22 19:27:19 -0600

    checkpoint

---
+++ CCM/Components/RichTextBoxTracingService.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Starcraft_Mod_Manager.Components
+{
+    partial class RichTextBoxTracingService
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.logVerbosityDropdownLabel = new System.Windows.Forms.Label();
+            this.logVerbosityDropdown = new System.Windows.Forms.ComboBox();
+            this.logBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // logVerbosityDropdownLabel
+            // 
+            this.logVerbosityDropdownLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.logVerbosityDropdownLabel.AutoSize = true;
+            this.logVerbosityDropdownLabel.BackColor = System.Drawing.Color.Transparent;
+            this.logVerbosityDropdownLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logVerbosityDropdownLabel.ForeColor = System.Drawing.Color.LightGray;
+            this.logVerbosityDropdownLabel.Location = new System.Drawing.Point(0, 0);
+            this.logVerbosityDropdownLabel.Margin = new System.Windows.Forms.Padding(10, 5, 10, 0);
+            this.logVerbosityDropdownLabel.Name = "logVerbosityDropdownLabel";
+            this.logVerbosityDropdownLabel.Padding = new System.Windows.Forms.Padding(5);
+            this.logVerbosityDropdownLabel.Size = new System.Drawing.Size(100, 26);
+            this.logVerbosityDropdownLabel.TabIndex = 34;
+            this.logVerbosityDropdownLabel.Text = "Log Verbosity";
+            this.logVerbosityDropdownLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // logVerbosityDropdown
+            // 
+            this.logVerbosityDropdown.BackColor = System.Drawing.Color.Black;
+            this.logVerbosityDropdown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.logVerbosityDropdown.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logVerbosityDropdown.ForeColor = System.Drawing.Color.LightGray;
+            this.logVerbosityDropdown.FormattingEnabled = true;
+            this.logVerbosityDropdown.ItemHeight = 16;
+            this.logVerbosityDropdown.Location = new System.Drawing.Point(3, 26);
+            this.logVerbosityDropdown.Margin = new System.Windows.Forms.Padding(0);
+            this.logVerbosityDropdown.Name = "logVerbosityDropdown";
+            this.logVerbosityDropdown.Size = new System.Drawing.Size(188, 24);
+            this.logVerbosityDropdown.TabIndex = 33;
+            this.logVerbosityDropdown.SelectedIndexChanged += new System.EventHandler(this.OnLogVerbosityDropdownSelectedIndexChanged);
+            // 
+            // logBox
+            // 
+            this.logBox.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logBox.Location = new System.Drawing.Point(3, 53);
+            this.logBox.Name = "logBox";
+            this.logBox.Size = new System.Drawing.Size(824, 122);
+            this.logBox.TabIndex = 32;
+            this.logBox.Text = "";
+            this.logBox.WordWrap = false;
+            // 
+            // RichTextBoxTracingService
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.logVerbosityDropdownLabel);
+            this.Controls.Add(this.logVerbosityDropdown);
+            this.Controls.Add(this.logBox);
+            this.Name = "RichTextBoxTracingService";
+            this.Size = new System.Drawing.Size(829, 178);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label logVerbosityDropdownLabel;
+        private System.Windows.Forms.ComboBox logVerbosityDropdown;
+        private System.Windows.Forms.RichTextBox logBox;
+    }
+}
---
+++ CCM/Components/RichTextBoxTracingService.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Forms;
+using ModManager.StarCraft.Base;
+using ModManager.StarCraft.Base.RichText;
+
+namespace Starcraft_Mod_Manager.Components
+{
+    public partial class RichTextBoxTracingService : UserControl, ITracingService
+    {
+        private static readonly Timer FlushTimer = new Timer();
+
+        public RichTextBoxTracingService()
+        {
+            InitializeComponent();
+
+            this.logVerbosityDropdown.Items.AddRange(
+                new string[]
+                {
+                    TraceLevel.Verbose.ToString(),
+                    TraceLevel.Info.ToString(),
+                    TraceLevel.Warning.ToString(),
+                    TraceLevel.Error.ToString(),
+                }
+            );
+
+            FlushTimer.Tick += this.OnTimerTick;
+            FlushTimer.Interval = 250; // 0.25 second
+            FlushTimer.Start();
+
+            this.AllTraceEvents = new List<TraceEvent>();
+            this.PendingTraceEventQueue = new Queue<TraceEvent>();
+            this.QueueThreshold = 100;
+            this.LockObject = new object();
+            this.RtfBody = string.Empty;
+            this.RtfColorTable = new ColorTable<TraceLevel>(Color.Black, traceLevel => traceLevel.ToColor());
+        }
+
+        private List<TraceEvent> AllTraceEvents { get; }
+
+        private Queue<TraceEvent> PendingTraceEventQueue { get; set; }
+
+        private int QueueThreshold { get; }
+
+        private TraceLevel TraceLevelThreshold { get; set; }
+
+        private object LockObject { get; }
+
+        private string RtfBody { get; set; }
+
+        private ColorTable<TraceLevel> RtfColorTable { get; }
+
+        // Event handlers
+
+        private void OnTimerTick(object sender, EventArgs e)
+        {
+            lock (this.LockObject)
+            {
+                if (!this.PendingTraceEventQueue.Any())
+                {
+                    return;
+                }
+
+                this.Flush();
+            }
+        }
+
+        private void OnLogVerbosityDropdownSelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (
+                !(this.logVerbosityDropdown.SelectedItem is string selectedItem)
+                || !Enum.TryParse(selectedItem, out TraceLevel selectedTraceLevel)
+            )
+            {
+                throw new ArgumentException("Unknown enum variant", nameof(selectedItem));
+            }
+
+            this.TraceLevelThreshold = selectedTraceLevel;
+
+            IEnumerable<TraceEvent> traceEventsMeetingThreshold = this.AllTraceEvents.Where(traceEvent =>
+                traceEvent.Level <= this.TraceLevelThreshold
+            );
+
+            this.RtfBody = this.GetRtfBodyFromTraceEvents(traceEventsMeetingThreshold);
+            this.logBox.Rtf = this.RegenerateRtf();
+            this.ScrollToEnd();
+        }
+
+        // ITracingService
+
+        public void TraceEvent(TraceEvent traceEvent)
+        {
+            lock (this.LockObject)
+            {
+                this.AllTraceEvents.Add(traceEvent);
+
+                if (traceEvent.Level > this.TraceLevelThreshold)
+                {
+                    return;
+                }
+
+                this.PendingTraceEventQueue.Enqueue(traceEvent);
+
+                if (this.PendingTraceEventQueue.Count >= this.QueueThreshold)
+                {
+                    this.Flush();
+                }
+            }
+        }
+
+        public void TraceMessage(
+            TraceLevel level,
+            string message,
+            [CallerFilePath] string callerFilePath = "",
+            [CallerMemberName] string callerMemberName = ""
+        )
+        {
+            this.TraceEvent(new TraceEvent(message, level, callerFilePath, callerMemberName));
+        }
+
+        public void TraceError(
+            string message,
+            [CallerFilePath] string callerFilePath = "",
+            [CallerMemberName] string callerMemberName = ""
+        )
+        {
+            this.TraceMessage(TraceLevel.Error, message, callerFilePath, callerMemberName);
+        }
+
+        public void TraceWarning(
+            string message,
+            [CallerFilePath] string callerFilePath = "",
+            [CallerMemberName] string callerMemberName = ""
+        )
+        {
+            this.TraceMessage(TraceLevel.Warning, message, callerFilePath, callerMemberName);
+        }
+
+        public void TraceInfo(
+            string message,
+            [CallerFilePath] string callerFilePath = "",
+            [CallerMemberName] string callerMemberName = ""
+        )
+        {
+            this.TraceMessage(TraceLevel.Info, message, callerFilePath, callerMemberName);
+        }
+
+        public void TraceVerbose(
+            string message,
+            [CallerFilePath] string callerFilePath = "",
+            [CallerMemberName] string callerMemberName = ""
+        )
+        {
+            this.TraceMessage(TraceLevel.Verbose, message, callerFilePath, callerMemberName);
+        }
+
+        // Other
+
+        private void Flush()
+        {
+            this.RtfBody += this.GetRtfBodyByDrainingQueue();
+
+            this.logBox.Rtf = this.RegenerateRtf();
+            this.ScrollToEnd();
+        }
+
+        private string RegenerateRtf()
+        {
+            return $@"{{\rtf1
+{this.RtfColorTable.Rtf}
+{this.RtfBody}
+}}";
+        }
+
+        private string GetRtfBodyByDrainingQueue()
+        {
+            string result = this.GetRtfBodyFromTraceEvents(this.PendingTraceEventQueue);
+
+            this.PendingTraceEventQueue.Clear();
+
+            return result;
+        }
+
+        private string GetRtfBodyFromTraceEvents(IEnumerable<TraceEvent> traceEvents)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (TraceEvent traceEvent in traceEvents)
+            {
+                if (traceEvent.Level > this.TraceLevelThreshold)
+                {
+                    continue;
+                }
+
+                int indexOfColorDef = this.RtfColorTable.GetColorIndex(traceEvent.Level);
+
+                // Escape the following: '\', '{', and '}'
+                string escapedMessage = traceEvent.Message.Replace(@"\", @"\\").Replace("{", @"\{").Replace("}", @"\}");
+
+                stringBuilder.AppendLine(
+                    // $"\\cf{indexOfColorDef} Hello world \\par"
+                    $"\\cf{this.RtfColorTable.DefaultColorIndex} {traceEvent.TimeStamp} - \\cf{indexOfColorDef} {traceEvent.Level} - {escapedMessage}\\par"
+                );
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        private void ScrollToEnd()
+        {
+            this.logBox.SelectionStart = this.logBox.Text.Length;
+            this.logBox.ScrollToCaret();
+        }
+    }
+}
